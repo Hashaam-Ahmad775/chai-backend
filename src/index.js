bfb710293b1be78a3d19be78b6c56b6db7303b49
Hashaam Ahmad <hashaamahmad490@gmail.com>
2025-02-23 08:54:34 +0500

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
 dotenv.config({
-    path:"./env"
+    path: "./env"
 })
 
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is listening at port: ${process.env.PORT}`);
+            app.on("Error", (error) => {
+                console.log("Error: ", error);
+                throw error
+            })
+
+        })
+    }
+    )
+    .catch((err) => {
+        console.log("MongoDB connection failed !!!!!  ", err);
+
+    })
 
 
 
---
+++ src/utils/Apiresponse.js
@@ -0,0 +1,8 @@
+class Apiresponse {
+    constructor () {
+        this.statusCode = this.statusCode
+        this.data = data
+        this.message  = message
+        this.success = statusCode < 400  
+    }
+}
