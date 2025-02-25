6ff9e4e6dfff38a803b70c017e52fc6a1752fea5
Hashaam Ahmad <hashaamahmad490@gmail.com>
2025-02-25 19:12:02 +0500

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,15 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// import routes
+import userRouter from "./routes/user.routes.js"
+
+//  routes declation
+app.use("/api/v1/users",userRouter) // routes are shifted to router file while routes will be declared.
+
+// This route will be formed
+    // http://localhost:8000/api/v1/users/register
+    // http://localhost:8000/api/v1//users/login
+    
+
 export {app}
